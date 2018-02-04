b44999983b50a02f7528dc9971a13d441849f137
Hugo <hugo.paez8712@gmail.com>
2018-02-04 15:09:30 -0600

    Instalacion de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyACzz7bvrjzOmJ7yRcrxwQctEQBBxnlCeU",
+    authDomain: "dgaesapp.firebaseapp.com",
+    databaseURL: "https://dgaesapp.firebaseio.com",
+    projectId: "dgaesapp",
+    storageBucket: "",
+    messagingSenderId: "1051666347913"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyACzz7bvrjzOmJ7yRcrxwQctEQBBxnlCeU",
+    authDomain: "dgaesapp.firebaseapp.com",
+    databaseURL: "https://dgaesapp.firebaseio.com",
+    projectId: "dgaesapp",
+    storageBucket: "",
+    messagingSenderId: "1051666347913"
+  }
 };
