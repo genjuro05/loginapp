f9ee97746132f73ed007711e2a4ec5c63270a001
Hugo <hugo.paez8712@gmail.com>
2018-02-04 19:05:14 -0600

    Actualizaciones del router

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,17 @@
 export const environment = {
   production: true,
   firebaseConfig: {
+    // tslint:disable-next-line:quotemark
     apiKey: "AIzaSyACzz7bvrjzOmJ7yRcrxwQctEQBBxnlCeU",
+    // tslint:disable-next-line:quotemark
     authDomain: "dgaesapp.firebaseapp.com",
+    // tslint:disable-next-line:quotemark
     databaseURL: "https://dgaesapp.firebaseio.com",
+    // tslint:disable-next-line:quotemark
     projectId: "dgaesapp",
+    // tslint:disable-next-line:quotemark
     storageBucket: "",
+    // tslint:disable-next-line:quotemark
     messagingSenderId: "1051666347913"
   }
 };
